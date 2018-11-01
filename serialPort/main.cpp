510e47b90c80ad2e7a82a415338d3fbfe9e483c0
Gang <776881718@qq.com>
2018-11-01 23:48:42 +0800

    ...

--- serialPort/main.cpp
+++ serialPort/main.cpp
@@ -1,4 +1,4 @@
-#include <QtGui/QApplication>
+#include <QtWidgets/QApplication>
 #include "mainwindow.h"
 #include"QTextCodec"
 
@@ -12,8 +12,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QTextCodec *codec = QTextCodec::codecForName("utf-8");
     QTextCodec::setCodecForLocale(codec);
-    QTextCodec::setCodecForCStrings(codec);
-    QTextCodec::setCodecForTr(codec);
+    //QTextCodec::setCodecForCStrings(codec);
+    //QTextCodec::setCodecForTr(codec);
 
     MainWindow w;
     w.setWindowTitle("SerialPort Communication!");
